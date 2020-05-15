49441676610aee9b4cd185a711f7d538de73236e
Ke Zhan <zksync@gmail.com>
2020-05-15 20:26:44 +1000

    Update draft.ps1
    Update Azure Powershell commands

--- draft.ps1
+++ draft.ps1
@@ -1,17 +1,15 @@
-param (
-  [string] $ResGrpName,
-  [string] $AppName,
-  [string] $SlotName,
-  [bool] $IsAdd
-)
+$ResGrpName = "xxx"
+$AppName = "xxx"
+$SlotName = "xxxxx"
+$IsAdd = $True
 
 Function GetResourceTypeAndName($SiteName, $Slot)
 {
     $ResourceType = "Microsoft.Web/sites"
     $ResourceName = $SiteName
     if (![string]::IsNullOrEmpty($Slot)) {
-        $ResourceType = "$($ResourceType)/slots"
-        $ResourceName = "$($ResourceName)/$($Slot)"
+        $ResourceType = "$ResourceType/slots"
+        $ResourceName = "$ResourceName/$Slot"
     }
     $ResourceType,$ResourceName
 }
@@ -39,9 +37,9 @@ Function AddIpToProperties($properties, $address, $subnetmask) {
 Function RemoveIpFromProperties($properties, $address) {
     $restrictions = $properties.ipSecurityRestrictions
     $newRestrictions = @()
-    Write-Host("address to remove $($address)")
+    Write-Host("address to remove $address")
     foreach ($restiction in $restrictions) {
-    Write-Host("source $($restiction.ipAddress)")
+    Write-Host("source $restiction.ipAddress")
 
         if($address -ne $restiction.ipAddress)  
         {
@@ -54,12 +52,12 @@ Function RemoveIpFromProperties($properties, $address) {
 
 $buildAgentIP = Invoke-RestMethod https://api.ipify.org/?format=json | Select -exp ip
     
-Write-Host("Build Agent IP Address: $($buildAgentIP)")
+Write-Host("Build Agent IP Address: $buildAgentIP")
 
 # Get resource type and resource name
 $ResourceType,$ResourceName = GetResourceTypeAndName $AppName $SlotName
 
-$r = Get-AzureRmResource -ResourceGroupName $ResGrpName -ResourceType $ResourceType/config -Name $ResourceName/web -ApiVersion 2016-08-01
+$r = Get-AzResource -ResourceGroupName $ResGrpName -ResourceType $ResourceType/config -Name $ResourceName/web -ApiVersion 2016-08-01
 
 # Get resource properties for IP restrictions
 $properties = $r.Properties
@@ -68,13 +66,13 @@ if($properties.ipSecurityRestrictions -eq $null){
 }
 
 if($IsAdd){
-    Write-Host("Adding Build Agent IP Address $($buildAgentIP)")
+    Write-Host("Adding Build Agent IP Address $buildAgentIP")
     AddIpToProperties $properties $buildAgentIP ""
 }else{
-    Write-Host("Remove Build Agent IP Address $($buildAgentIP)")
+    Write-Host("Remove Build Agent IP Address $buildAgentIP")
     RemoveIpFromProperties $properties $buildAgentIP
 }
 
-Set-AzureRmResource -Force -ResourceGroupName  $ResGrpName -ResourceType $ResourceType/config -Name $ResourceName/web -PropertyObject $properties -ApiVersion 2016-08-01
+Set-AzResource -Force -ResourceGroupName  $ResGrpName -ResourceType $ResourceType/config -Name $ResourceName/web -PropertyObject $properties -ApiVersion 2016-08-01
 
 Start-Sleep -s 10
